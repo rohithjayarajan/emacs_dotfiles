8b8e160daf2f2362fe079efbbd27d041db54fef8
Rohith Jayarajan <rjayarajan@autox.ai>
2020-03-27 17:56:50 -0700

    add init.el

---
+++ .emacs.d/init.el
@@ -0,0 +1,42 @@
+
+;;; -*- lexical-binding: t -*-
+
+;;;add MELPA
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (when no-ssl (warn "\
+Your version of Emacs does not support SSL connections,
+which is unsafe because it allows man-in-the-middle attacks.
+There are two things you can do about this warning:
+1. Install an Emacs version that does support SSL and be safe.
+2. Remove this warning from your init file so you won't see it again."))
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+  ;; and `package-pinned-packages`. Most users will not need or want to do this.
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  )
+(package-initialize)
+
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+(if (not (package-installed-p 'use-package))
+    (progn
+      (package-refresh-contents)
+      (package-install 'use-package)))
+(require 'use-package)
+
+;; Load all files from ~/.emacs.d/config directory
+;; Doesn't support nested dirs
+(dolist
+    (file
+     (directory-files
+      (concat (expand-file-name user-emacs-directory) "config")
+      t
+      "^.[^#].+el$"))
+  (load-file file))
+
+;;init ends here
+
