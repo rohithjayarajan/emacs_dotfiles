c9aa79792796b8507c2f5b58fd166c0ee332d98d
rohithjayarajan <rohith.jayarajan@gmail.com>
2020-03-28 18:06:36 -0700

    refactor

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -31,19 +31,3 @@
 ;;;end config
 
 
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (evil-collection solarized-theme evil iedit anzu comment-dwim-2 ws-butler dtrt-indent clean-aindent-mode yasnippet undo-tree volatile-highlights helm-gtags helm-projectile helm-swoop helm zygospore projectile company use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
