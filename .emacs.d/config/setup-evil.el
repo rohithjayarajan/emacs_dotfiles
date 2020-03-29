8ec6b05a818c4d9c1cfd090e2e52ebe3f7d0b306
rohithjayarajan <rohith.jayarajan@gmail.com>
2020-03-28 18:26:01 -0700

    update evil: keep emacs navigation defaults

--- .emacs.d/config/setup-evil.el
+++ .emacs.d/config/setup-evil.el
@@ -1,23 +1,17 @@
-(use-package evil
-  :ensure t
-  :defer .1 ;; don't block emacs when starting, load evil immediately after startup
-  :init
-  (setq evil-want-integration nil) ;; required by evil-collection
-  (setq evil-search-module 'evil-search)
-  (setq evil-ex-complete-emacs-commands nil)
-  (setq evil-vsplit-window-right t) ;; like vim's 'splitright'
-  (setq evil-split-window-below t) ;; like vim's 'splitbelow'
-  (setq evil-shift-round nil)
-  (setq evil-want-C-u-scroll t)
-  :config
-  (evil-mode)
+;; Download Evil
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+
+;; Enable Evil
+(require 'evil)
+(evil-mode 1)
 
 ;; vim-like keybindings everywhere in emacs
-(use-package evil-collection
-  :after evil
-  :ensure t
-  :config
-  (evil-collection-init))
-(message "Loading evil-mode...done"))
+;(use-package evil-collection
+;  :after evil
+;  :ensure t
+;  :config
+;  (evil-collection-init))
+;(message "Loading evil-mode...done"))
 
 (provide 'setup-evil)
